e8903578065978a8b9d7182b45af41b1fb280aa9
David Zeit <dzeit216@gmail.com>
2018-04-17 19:02:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,10 @@ def badge_maker(name)
  "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
-
+def batch_badge_creator(array)
+  array.each do |name|
+   badge_maker(name)
+  end
 end
 
 def assign_rooms
